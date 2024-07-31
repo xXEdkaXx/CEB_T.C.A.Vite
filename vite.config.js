5224d96e1a3af28c87bb2200219b1f03f994c76d
Eduardo <eduardoalvarado2023@gmail.com>
2024-07-31 00:53:30 -0600

    vite

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ import handlerBarsContext from './variables.js';
 
 
 export default defineConfig({
-    base: "/CEB_T.C.A.Vite/",
+    base: "/https://xxedkaxx.github.io/CEB_T.C.A.Vite//",
     appType: 'mpa',
     build: {
         rollupOptions: {
